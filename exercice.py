25afcff731d9f2fea44745c92b8b7a6b27318a4e
simonhache <simonhache17@gmail.com>
2020-10-20 21:14:42 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -16,7 +16,7 @@ def reverse_data(data: list = None):
 
 def delete_nth_from_stack(data: Stack, position: int) -> Stack:
     
-    return Stack()
+    return Stack(data.pop())
 
 
 def delete_nth_from_queue(data: Queue, position: int) -> Queue:
