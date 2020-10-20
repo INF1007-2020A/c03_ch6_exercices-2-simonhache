fc3fff1afdbdc43ee5ca151013ad6f3d2b5712d0
simonhache <simonhache17@gmail.com>
2020-10-20 21:14:21 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -15,7 +15,7 @@ def reverse_data(data: list = None):
 
 
 def delete_nth_from_stack(data: Stack, position: int) -> Stack:
-    # TODO: Supprimer le énième (position) élément de data et retourner la nouvelle structure de données.
+    
     return Stack()
 
 
