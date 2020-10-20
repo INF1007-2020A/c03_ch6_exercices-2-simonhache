d20d8d533b886acb76aeb5b576e6a910056d6182
simonhache <simonhache17@gmail.com>
2020-10-20 21:20:28 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -15,7 +15,7 @@ def reverse_data(data: list = None):
 
 
 def delete_nth_from_stack(data: Stack, position: int) -> Stack:
-    print(type(list(Stack)))
+    print(type(list(data)))
     #newData = [el for el,idx in enumerate(list(data)) if idx != position]
     return #Stack(newData)
 
