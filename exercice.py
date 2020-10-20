d0fe174273092624b4106bfce05975c16a6b5f2c
simonhache <simonhache17@gmail.com>
2020-10-20 21:10:14 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -11,7 +11,7 @@ def reverse_data(data: list = None):
     # et les retourner en ordre inverse, sans utiliser de liste.
 
     if data is None:
-        data = input("")
+        data = input("Veuillez entrer ")
 
     reversed_data = None  # Stocker le résultat ici
 
